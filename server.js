b158de954b6fb9e6ba1b68e8216d98cd8a887cc6
Jacob Ogren <jogren0044@gmail.com>
2019-10-07 14:47:54 -0600

    Add get all projects endpoint

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', (request, response) => {
   response.send('Welcome to Palette Picker');
 });
 
+app.get('/api/v1/projects', (request, response) => {
+  console.log('return all projects')
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on PORT ${app.get('port')}.`)
 })
