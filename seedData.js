697322de78fa249a379f5269d8ab67a21b37bf65
Jacob Ogren <jogren0044@gmail.com>
2019-10-08 13:31:27 -0600

    Add mock data to seedData.js

---
+++ seedData.js
@@ -0,0 +1,36 @@
+const seedData = [
+  {
+    name: 'Winter',
+    palettes: [
+      {  
+        name: 'Christmas Theme',
+        color1: '#800000',
+        color2: '#228B22',
+        color3: '#FFFFFF',
+        color4: '#FFFFFF',
+        color5: '#FFFFFF'
+      },
+      {
+        name: 'Hanukkah Theme',
+        color1: '#C0C0C0',
+        color2: '#3895D3',
+        color3: '#FFFFFF',
+        color4: '#FFFFFF',
+        color5: '#FFFFFF'
+      }
+    ]
+  },
+  {
+    name: 'Fall',
+    palettes: [
+      {
+        name: 'Halloween Theme',
+        color1: '#FF4500',
+        color2: '#000000',
+        color3: '#FFFFFF',
+        color4: '#FFFFFF',
+        color5: '#FFFFFF'
+      }
+    ]
+  },
+]
